700235d0ed68eed7e269886976c115c04de3f2eb
sureeth14 <82883429+sureeth14@users.noreply.github.com>
2023-03-02 11:22:46 +0530

    Add files via upload

--- wallet/src/main/java/com/wallet/WalletJpaRepository.java
+++ wallet/src/main/java/com/wallet/WalletJpaRepository.java
@@ -1,13 +1,25 @@
 package com.wallet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface WalletJpaRepository extends JpaRepository<WalletDto,Integer> {
-//    WalletDto createWallet(WalletDto newWallet);
-//    WalletDto getWalletById(Integer  walletId);
-//    WalletDto updateWallet(WalletDto wallet);
-//    WalletDto deleteWalletById(Integer walletId);
-//    Collection<WalletDto> getAllWallets();
+
+    //creation of custom queries by method name using keyWords
+
+    List<WalletDto> findByName(String name);
+    List<WalletDto> findByNameContaining(String name);
+
+
+    //by writing JPQL queries
+
+    @Query("SELECT wallet FROM WalletDto wallet")
+    List<WalletDto> getAllWallets();
+
+    @Query("SELECT wallet FROM WalletDto wallet WHERE wallet.name LIKE : name ")
+    List<WalletDto> getAllWalletsHavingNameLike(String name);
+
 }
